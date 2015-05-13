bad519e406e930cc4c82d0cd95bc50bf2126b06b
陈瑞红 <602882411@qq.com>
2015-05-13 17:19:07 +0800

    初始导入

---
+++ BuildingControl/src/com/building/service/ifc/SmUserServiceIFC.java
@@ -0,0 +1,40 @@
+package com.building.service.ifc;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.building.model.SmUser;
+
+public interface SmUserServiceIFC {
+	
+	/**
+	  * @Description: 获取实体列表 
+	  */
+	List<SmUser> querySmUser4List(HttpServletRequest request, SmUser smUser);
+	
+	
+	/**
+	  * @Description: 查看实体对象 
+	  */
+	SmUser querySmUser4Bean(SmUser smUser);
+	
+	
+	/**
+	  * @Description: 保存添加实体对象 
+	  */
+	void saveAddSmUser(SmUser smUser);
+	
+	
+	/**
+	  * @Description: 保存编辑实体对象 
+	  */
+	void saveEditSmUser(SmUser smUser);
+	
+	
+	/**
+	  * @Description: 删除实体对象
+	  */
+	void delSmUser(SmUser smUser);
+	
+}
